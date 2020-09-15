b928fea4d16b73e74ad56be20f3eee8bb956fb64
Alluisio Silva <alluisio.silva@ipdec.org>
2020-09-15 15:43:23 -0400

    Finalização do curso de React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import { Container, Typography } from "@material-ui/core";
+import "fontsource-roboto";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <h1>Formulário de cadastro</h1>
-        <FormularioCadastro />
-      </Fragment>
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center">
+          Formulário de cadastro
+        </Typography>
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
+      </Container>
     );
+  }
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados);
+}
+
+function validarCPF(cpf) {
+  if (cpf.length !== 11) {
+    return { valido: false, texto: "CPF deve ter 11 dígitos." };
+  } else {
+    return { valido: true, texto: "" };
   }
 }
 
